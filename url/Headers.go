0353dfc866e75d847d563dc59e0c190f1e479d17
LeeGene <1670044143@qq.com>
2022-09-27 16:30:39 +0800

    过滤注释字段以及过滤伪头部字段

--- url/Headers.go
+++ url/Headers.go
@@ -8,7 +8,9 @@ import (
 
 // 初始化Headers结构体
 func NewHeaders() *http.Header {
-	return &http.Header{}
+	return &http.Header{
+		http.PHeaderOrderKey: {":method", ":authority", ":scheme", ":path"},
+	}
 }
 
 // 解析Headers字符串为结构体
@@ -18,10 +20,9 @@ func ParseHeaders(headers string) *http.Header {
 	lines := strings.Split(headers, "\n")
 	for _, header := range lines {
 		header = strings.TrimSpace(header)
-		if header == "" {
+		if header == "" || strings.Index(header, ":") == 0 || strings.Index(header, "/") == 0 || strings.Index(header, "#") == 0 {
 			continue
 		}
-		header = strings.TrimLeft(header, ":")
 		keyValue := strings.SplitN(header, ":", 2)
 		if len(keyValue) != 2 {
 			panic(errors.New("该字符串不符合http头部标准！"))
